11b8e0d35a9ccdb916f3df4e0dc661178c6583f3
Ricardo Cantó Abad <rcanto@users.noreply.github.com>
2017-03-23 12:59:13 +0100

    Add files via upload
    Revisión inicial

---
+++ hmg.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.util.*;
+
+public class hmg
+{
+	public static void main(String[] args) 
+	{
+		Vector<String> noms=new Vector<String>();                                                     
+		JFrame frame = new JFrame("Ventana Hola Mundo");
+		noms.add("Ricardo");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		// SE CREA LA ETIQUETA, INICIALMENTE INDEPENDIENTE DE LA VENTANA
+		int num = (int)((Math.random())*noms.size());
+		JLabel label= new JLabel("Hola " + noms.get(num));
+		// SE AÑADE LA ETIQUETA A LA VENTANA
+		frame.getContentPane().add(label);
+		// ESTABLECE EL TAMAÑO DE LA VENTANA POR DEFECTO AL VALOR MÍNIMO DE SUS COMPONENTES
+		frame.pack();
+		// CENTRAMOS LA VENTANA CON EL VALOR NULL
+		frame.setLocationRelativeTo(null);
+		// ADEMÁS, HAY QUE HACERLO VISIBLE
+		frame.setVisible(true);
+	}
+}
